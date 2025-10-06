98303d7e7b2f4f5400b0bfce2396cede7b2aba16
Chloestfy <chloe.dijoux@hotmail.com>
2025-10-06 12:08:26 +0200

    Update Usercontroller.php

--- mvc/controller/Usercontroller.php
+++ mvc/controller/Usercontroller.php
@@ -1,13 +1,1 @@
-<?php
 
-require_once __DIR__ . '/../model/User.php';
-
-
-class Usercontroller
-{
-    public function showUser()
-    {
-        $user = new User("chloe");
-        require_once __DIR__ . '/../view/userView.php';
-    }
-}
