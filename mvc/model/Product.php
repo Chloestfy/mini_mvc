7117ad5e908845415cfa51c9b3ab945e5804bfd6
Chloestfy <chloe.dijoux@hotmail.com>
2025-10-06 12:09:51 +0200

    Update Product.php

--- mvc/model/Product.php
+++ mvc/model/Product.php
@@ -1,12 +1,1 @@
-<?php
-class Product
-{
-    public $title;
-    public $price;
 
-    public function __construct($title, $price)
-    {
-        $this->title = $title;
-        $this->price = $price;
-    }
-}
